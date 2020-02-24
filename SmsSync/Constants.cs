7d7fbf8405c0e663ad6228dc16e5e27db7dbfa7d
misikdmitriy <misik.dmitriy@gmail.com>
2020-02-25 00:13:10 +0200

    update source

--- SmsSync/Constants.cs
+++ SmsSync/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Source = "KYIVSTAR";
+        public const string Source = "Kyivstar";
         public const string BearerType = "sms";
         public const string ContentType = "text/plain";
         public const string ServiceType = "False";
