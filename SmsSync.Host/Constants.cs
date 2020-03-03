1643db037965055a3c4278e4ba4f5d66ef32f2f6
dmytro.m <dmytro.m@sbtech.com>
2020-03-03 12:40:28 +0200

    change source type

--- SmsSync.Host/Constants.cs
+++ SmsSync.Host/Constants.cs
@@ -7,7 +7,7 @@
             public const string Source = "Kyivstar";
             public const string BearerType = "sms";
             public const string ContentType = "text/plain";
-            public const string ServiceType = "False";
+            public const string ServiceType = "false";
         }
 
         public static class States
