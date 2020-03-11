3fd95f38d08a730654a7a358c9c81137ec9ce0d6
dmytro.m <dmytro.m@sbtech.com>
2020-03-11 17:19:12 +0200

    add '380' to phone number

--- SmsSync.Host/Templates/PhoneNumberBuilder.cs
+++ SmsSync.Host/Templates/PhoneNumberBuilder.cs
@@ -7,7 +7,21 @@ namespace SmsSync.Templates
     {
         public Task<string> Build(DbSms sms)
         {
-            return Task.FromResult(sms.ClientPhone);
+            var phoneNumber = sms.ClientPhone;
+            
+            // 1. Remove from number first '+' or '0'
+            // +380501234567 -> 380501234567 
+            // 0501234567 -> 501234567
+            var result = phoneNumber.TrimStart('+').TrimStart('0');
+
+            // 2. Prepend '380' at start if number start with another symbols
+            // 501234567 -> 380501234567
+            if (!result.StartsWith("380"))
+            {
+                result = $"380{result}";
+            }
+            
+            return Task.FromResult(result);
         }
     }
 }
